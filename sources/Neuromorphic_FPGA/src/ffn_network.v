3c616c6a7d53dfd6cb180e86dac6d45906f5b393
rafie <amirali.rafie.03@gmail.com>
2026-08-17 23:02:09 +0330

    updated the testbench and the verilog code to be compatable with tang nano 9k

--- sources/Neuromorphic_FPGA/src/ffn_network.v
+++ sources/Neuromorphic_FPGA/src/ffn_network.v
@@ -1,4 +1,5 @@
-// simple network with sift bsed decay
+// simple network with sift bsed decay 
+// made to run on tang nano 9k
 
 module ffn_22_netwok #(
     parameter w1 = 8'sd5,
@@ -6,33 +7,51 @@ module ffn_22_netwok #(
     parameter w3 = 8'sd7,
     parameter w4 = 8'sd8,
     parameter threshold = 11'sd50,
-    parameter beta_shift = 3
+    parameter beta_shift = 4
 )(
-    input wire clk,
-    input wire reset,
+    input wire sys_clk,
+//    input wire reset,
     input wire  in1,
     input wire in2,
     output reg out1,
     output reg out2
 );
 
+
+
 reg signed [9:0] u1;
 reg signed [9:0] u2;
+
+reg [24:0] count;
+reg tick;
 
 wire signed [10:0] u1_new;
 wire signed [10:0] u2_new;
 
 wire signed [10:0] u1_decayed;
 wire signed [10:0] u2_decayed;
+wire clk;
 
 // u1: 10 bit to 11 bit
 // w1,w3: 8 bit to 11 bit
 assign u1_new = {u1[9],u1} + (in1 ? {{3{w1[7]}}, w1} : 11'sd0) + (in2 ? {{3{w3[7]}}, w3} : 11'sd0) - ({u1[9],u1} >> beta_shift);
 assign u2_new = {u2[9],u2} + (in2 ? {{3{w2[7]}}, w2} : 11'sd0) + (in2 ? {{3{w4[7]}}, w4} : 11'sd0) - ({u1[9],u1} >> beta_shift);
-
+assign clk = tick;
 
 //assign u1_sum = $signed(u1) + $signed(w1);
 //assign u1_sum = $signed(u1) + $signed(w1);
+
+reg reset = 1;
+
+always @(posedge sys_clk) begin
+        if (count == 25'd27000000 - 1) begin
+            count <= 25'd0;
+            tick  <= 1'b1; // Active for 1 cycle of 27MHz
+        end else begin
+            count <= count + 1'b1;
+            tick  <= 1'b0;
+        end
+end
 
 always @(posedge clk) begin
     
@@ -41,6 +60,7 @@ always @(posedge clk) begin
         u2   <= 10'sd0;
         out1 <= 1'b0;
         out2 <= 1'b0;
+        reset = 0;
      end
 
     // calculating potentils
--- testbemch/test_ffn_verilog.py
+++ testbemch/test_ffn_verilog.py
@@ -5,34 +5,27 @@ from cocotb.triggers import FallingEdge, RisingEdge, ReadOnly
 
 @cocotb.test(timeout_time=100, timeout_unit="ns")
 async def test_ffn(dut):
-    """Test the feed-forward network."""
-
-    # Initialize inputs before starting the test
     dut.in1.value = 0
     dut.in2.value = 0
-    dut.reset.value = 1
+    #dut.reset.value = 1
 
-    # 2 ns clock period: 1 ns low, 1 ns high
     clock = Clock(dut.clk, 2, unit="ns")
     clock.start(start_high=False)
 
-    dut.reset.value = 1
+    #dut.reset.value = 1
 
-    # Wait for the circuit to process the initial inputs
     await RisingEdge(dut.clk)
     await ReadOnly()
 
     cocotb.log.info("Initial out1 is %s", dut.out1.value)
     cocotb.log.info("Initial out2 is %s", dut.out2.value)
 
-    # Change inputs away from the active rising edge
     await FallingEdge(dut.clk)
-    dut.reset.value = 0
+    #dut.reset.value = 0
 
     dut.in1.value = 1
     dut.in2.value = 1
 
-    # Wait for the next clock edge that samples the inputs
     await RisingEdge(dut.clk)
     await ReadOnly()
 
@@ -41,9 +34,8 @@ async def test_ffn(dut):
         
         cocotb.log.info("u1 is %s", dut.u1.value)
         cocotb.log.info("out1 is %s", dut.out1.value)
+        cocotb.log.info("u2 is %s", dut.u2.value)
+        cocotb.log.info("out2 is %s", dut.out2.value)
             
         await RisingEdge(dut.clk)
         await ReadOnly()
-
-    cocotb.log.info("After inputs changed, out1 is %s", dut.out1.value)
-    cocotb.log.info("After inputs changed, out2 is %s", dut.out2.value)
